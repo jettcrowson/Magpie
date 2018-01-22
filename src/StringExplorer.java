b434340d75ae17074550303934c90423a530fcba
Jett Crowson <jettcrowson@gmail.com>
2018-01-22 11:43:36 -0700

    Activity 3

--- src/StringExplorer.java
+++ src/StringExplorer.java
@@ -20,6 +20,15 @@ public class StringExplorer {
 
 		// Try other methods here:
 
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("Index not found: " + notFoundPsn);
+
+		System.out.println("We will now demonstrate the difference between indexOf with one parameter vs two");
+		int regularIndexOf = sample.indexOf("o");
+		int twoParamIndexOf = sample.indexOf("o", 14);
+		System.out.println("sample.indexOf(\"o\") = " + regularIndexOf);
+		System.out.println("sample.indexOf(\"o\", 14) = " + twoParamIndexOf);
+		System.out.println("It will ignore any instances before the starting index, meaning the first one uses the o in 'brown' while the second uses the o in 'over'");
 	}
 
 }
